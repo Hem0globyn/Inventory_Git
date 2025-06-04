f19053ea8c8d41534ec6659f9264d4e942cf63dc
Sungju Kim <tjdwn0885@naver.com>
2025-06-04 20:23:45 +0900

    FEAT: 플레이어스탯 SO 만듦
    플레이어스탯의 데이터를 저장했습니다

---
+++ Assets/Script/CharacterStatus.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewData", menuName = "MyGame/StatData")]
+public class CharacterStatus : ScriptableObject
+{
+    [Header("ATK")]
+    public int attack;
+
+    [Header("DEF")]
+    public int defense;
+
+    [Header("HP")]
+    public int healthPoint;
+
+    [Header("Crit")]
+    public float critical;
+}
